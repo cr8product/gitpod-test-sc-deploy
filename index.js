02a2703cd7da25629e01f94a5b72ae0b064fa3ee
Begum SA <begumsener94@gmail.com>
2023-05-21 13:07:11 +0000

    Get block number

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+import { ethers } from "ethers";
+
+const provider= new ethers.InfuraProvider();
+console.log("Block no: ", await provider.getBlockNumber());
+
+
