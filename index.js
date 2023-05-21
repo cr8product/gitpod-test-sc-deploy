d1d3830673b9f82d18b8123df1f1b8e3005f7174
Begum SA <begumsener94@gmail.com>
2023-05-21 13:33:29 +0000

    getbalance from a wallet

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //console.log("Hello world!");
 import { ethers } from "ethers";
 
-const provider= new ethers.InfuraProvider();
+const provider= new ethers.InfuraProvider("matic");
 console.log("Block no: ", await provider.getBlockNumber());
 
 
+const wallet= "0x6b0A19082676620a022d0FB2fE8D0DF80c5b05B9";
+const walletbalance= await  provider.getBalance(wallet);
+console.log(ethers.formatEther(walletbalance), "MATIC");
